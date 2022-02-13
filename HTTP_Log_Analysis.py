ebf9e80fc22cafe1a0784de9a313084b42532834
Jax <98493070+space-cadet12@users.noreply.github.com>
2022-02-12 19:54:40 -0600

    Update HTTP_Log_Analysis.py

--- HTTP_Log_Analysis.py
+++ HTTP_Log_Analysis.py
@@ -5,14 +5,26 @@ This program will fetch a specific http log file from the specified address.
 Using this log file, it will be parsed to show the total number of logs in the past 6 months, and all time logs.
 '''
 
-retrieved == False 
+import requests #necessary module
+import os # necessary module
+from os.path import exists #neccesary module
 
-if retrieved == False: # conditional statement so the program will be able to determine if it needs to fetch the file or not. 
+cwd = os.getcwd()
+cwd += '\http_access_log.txt'
 
-  import requests #necessary module
+file_exists = exists(cwd)
 
-  url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-  r = requests.get(url, allow_redirects = True) # This is the object that the fecth is associted
+if file_exists == False: # conditional statement so the program will be able to determine if it needs to fetch the file or not. 
 
-  f = open('name', 'r').write(r.content)
-  print(f.read)
+    url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+    r = requests.get(url, allow_redirects = True) 
+
+    f = open('http_access_log.txt', 'wb')
+    for chunk in r.iter_content(chunk_size = 8192):
+        if chunk:
+            f.write(chunk)
+    f.close()
+
+'''with open('http_access_log.txt', 'r') as f: 
+    #print(f.read())'''
+# The above is the part that makes my IDE freeze. Don't think it should be doing that. 
