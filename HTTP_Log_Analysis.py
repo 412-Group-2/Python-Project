c64f37eee9c70a6e42d8ef4e5b81b3268333a6ef
Jax <98493070+space-cadet12@users.noreply.github.com>
2022-02-12 22:28:22 -0600

    Update HTTP_Log_Analysis.py

--- HTTP_Log_Analysis.py
+++ HTTP_Log_Analysis.py
@@ -10,21 +10,27 @@ import os # necessary module
 from os.path import exists #neccesary module
 
 cwd = os.getcwd()
-cwd += '\http_access_log.txt'
+cwd += '\http_access_log.txt' # obtain file path 
 
-file_exists = exists(cwd)
+file_exists = exists(cwd) # check if file exists
 
 if file_exists == False: # conditional statement so the program will be able to determine if it needs to fetch the file or not. 
 
     url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-    r = requests.get(url, allow_redirects = True) 
+    r = requests.get(url, allow_redirects = True) # create object to connect to and then fetch webpage information
 
     f = open('http_access_log.txt', 'wb')
     for chunk in r.iter_content(chunk_size = 8192):
         if chunk:
             f.write(chunk)
-    f.close()
+    f.close() # creation and closing of the file locally
 
 with open('http_access_log.txt') as f: 
     for line in f:
-        print(line.rstrip())
+        print(line.rstrip()) # re-opening file and then iterating through each line 1 time. 
+        
+'''
+
+From here I would figure out how to put each line item into a list. 
+
+'''
