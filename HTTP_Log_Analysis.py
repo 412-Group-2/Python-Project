2862d0338fae92cebfe28fe40bd2904c3bf388dd
Jax <98493070+space-cadet12@users.noreply.github.com>
2022-02-22 21:55:24 -0600

    Update HTTP_Log_Analysis.py

--- HTTP_Log_Analysis.py
+++ HTTP_Log_Analysis.py
@@ -2,7 +2,7 @@
 Created by Group 2
 
 This program will fetch a specific http log file from the specified address.
-Using this log file, it will be parsed to show the total number of logs in the past 6 months, and all time logs.
+Using this log file, it will be parsed to display various desired outputs.
 '''
 
 import requests
